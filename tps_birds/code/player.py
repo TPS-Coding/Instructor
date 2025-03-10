8be950c0002f77bcdfa9da6f5b8f0eb1b1de9d7b
TPScoding <TPScoding@tuxedoparkschool.org>
2025-03-10 09:50:59 -0400

    Update player.py

--- tps_birds/code/player.py
+++ tps_birds/code/player.py
@@ -7,14 +7,14 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load("../graphics/birds/Bird01/Bird01_00.png")
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_frect(center=(WINDOW_WIDTH*0.2, WINDOW_HEIGHT/2))
-        self.direction = 0
+        self.velocity = 0
         self.pos = pygame.math.Vector2(self.rect.topleft)
         self.gravity = 10
 
     def apply_gravity(self,dt):
         ## direction here is actually velocity, should be self.velocity
-        self.direction += self.gravity * dt
-        self.pos.y += self.direction * dt
+        self.velocity += self.gravity * dt
+        self.pos.y += self.velocity * dt
         self.rect.y = round(self.pos.y)
 
     def jump(self):
